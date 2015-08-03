d9443fc21384bf9ee29b9ae522b47cbb85cdcec1
Russell Dillin <grgrsslldlln@gmail.com>
2015-08-03 18:37:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### About
+
+In an effort to better learn the functional programming paradigm,
+and more specifically, the [Haskell programming language](https://www.haskell.org), I am working through some programming puzzles in my spare time. These problems are from the course [COMP 349: Applied Algorithms](http://www.harding.edu/academics/colleges-departments/sciences/computer-science/courses-offered) at [Harding University](http://www.harding.edu).
+
+An explanation of the problem is located in the `info` folder of this repo.
+Source code is located in the `src` folder and is licened under the GNU GPLv3.
+Copyright information is located in the `COPYING` file in the root of this repo.
+Additional information on the GNU GPLv3 license is located at http://www.gnu.org/licenses/.  
+
+_*Coursework is only allowed in C++ or Java so this isn't too helpful for current students.*_
---
+++ src/sched.hs
@@ -0,0 +1,122 @@
+{- Copyright © 2015 Russell Dillin
+
+sched is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+sched is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sched. If not, see <http://www.gnu.org/licenses/>. -}
+
+getGreatestDiff :: [(Int, Int)] -> (Int, Int) -> Int
+getGreatestDiff ys t = getGreatestDiff' ys t 0
+
+getGreatestDiff' :: [(Int, Int)] -> (Int, Int) -> Int -> Int
+getGreatestDiff' [] t diff = diff
+getGreatestDiff' ys t diff
+    | sub > diff = getGreatestDiff' (tail ys) t sub
+    | otherwise  = getGreatestDiff' (tail ys) t diff
+    where
+      sub = snd t - snd (head ys)
+
+diffForBest :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)]
+diffForBest ys d t = diffForBest' ys d t []
+
+diffForBest' :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+diffForBest' [] d t zs = zs
+diffForBest' ys d t zs
+    | h > f && (h - f) == diff = diffForBest' (tail ys) d t (t : zs)
+    | otherwise                = diffForBest' (tail ys) d t (head ys : zs)
+    where
+      f    = snd (head ys)
+      h    = snd t
+      diff = getGreatestDiff ys t 
+
+keepTheBest :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)]
+keepTheBest ys d t = keepTheBest' ys d t []
+
+keepTheBest' :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+keepTheBest' [] d t zs
+    | length zs == d = diffForBest zs d t
+    | otherwise      = (t : zs)
+keepTheBest' ys d t zs
+    | h > f && e <= g && g <= count = keepTheBest' (tail ys) d t zs
+    | otherwise                     = keepTheBest' (tail ys) d t (head ys : zs)
+    where
+      e     = fst (head ys)
+      f     = snd (head ys)
+      g     = fst t
+      h     = snd t
+      count = countDeadlines ys g
+
+buildBestOrder :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)]
+buildBestOrder ys d t = buildBestOrder' ys d t []
+
+buildBestOrder' :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+buildBestOrder' [] d t zs
+    | g /= count = t : zs
+    | otherwise  = zs
+    where
+      g     = fst t
+      count = countDeadlines zs g
+buildBestOrder' ys d t zs
+    | h > f && e == g && g == count = buildBestOrder' (tail ys) d t zs
+    | otherwise                     = buildBestOrder' (tail ys) d t (head ys : zs)
+    where
+      e     = fst (head ys)
+      f     = snd (head ys)
+      g     = fst t
+      h     = snd t
+      count = countDeadlines ys g
+
+bestOrders :: [(Int, Int)] -> Int -> (Int, Int) -> [(Int, Int)]
+bestOrders [] d t = [t]
+bestOrders ys d t
+    | l  > 0 && l < d = buildBestOrder ys d t
+    | l == d          = keepTheBest ys d t
+    where
+      l = length ys
+
+getBestOrders :: [(Int, Int)] -> Int -> [(Int, Int)]
+getBestOrders xs d = getBestOrders' xs d []
+
+getBestOrders' :: [(Int, Int)] -> Int -> [(Int, Int)] -> [(Int, Int)]
+getBestOrders' [] d ys     = ys
+getBestOrders' (x:xs) d ys = getBestOrders' xs d (bestOrders ys d x)
+
+countDeadlines :: [(Int, Int)] -> Int -> Int
+countDeadlines xs d = countDeadlines' xs d 0
+
+countDeadlines' :: [(Int, Int)] -> Int -> Int -> Int
+countDeadlines' [] d n     = n
+countDeadlines' (x:xs) d n
+    | y == d               = countDeadlines' xs d (n + 1)
+    | otherwise            = countDeadlines' xs d n
+    where
+      y = fst x
+
+getProfit :: [(Int, Int)] -> Int
+getProfit xs = getProfit' xs 0
+
+getProfit' :: [(Int, Int)] -> Int -> Int
+getProfit' [] n     = n
+getProfit' (x:xs) n = getProfit' xs (n + snd x)
+
+getDeadline :: [(Int, Int)] -> Int
+getDeadline xs = getDeadline' xs 0
+
+getDeadline' :: [(Int, Int)] -> Int -> Int
+getDeadline' [] n     = n
+getDeadline' (x:xs) n
+    | y > n           = getDeadline' xs y
+    | otherwise       = getDeadline' xs n
+    where
+      y = fst x
+
+maxProfit :: [(Int, Int)] -> Int
+maxProfit xs = getProfit $ getBestOrders xs (getDeadline xs)
